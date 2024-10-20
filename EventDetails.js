5ac18d9cbb697d059da654578fd9801e1dc60ad2
praveenaidu19 <140714918+praveenaidu19@users.noreply.github.com>
2024-10-21 04:37:13 +0530

    Create EventDetails.js

---
+++ EventDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const EventDetails = () => {
+  return (
+    <div>
+      <h1>School Reunion 2024</h1>
+      <p>Date: December 20th, 2024</p>
+      <p>Location: School Auditorium</p>
+      <p>Agenda: Meet & Greet, Speeches, Dance, and Dinner</p>
+      <button>RSVP Now</button>
+    </div>
+  );
+};
+
+export default EventDetails;
